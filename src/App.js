1eb558964eade7c3496415c1c09d605ac40f3041
Prasant Kumar Sahu <prasantkumarsahu0103@gmail.com>
2024-06-25 22:36:29 +0530

    update 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import pic from './pic-1.jpg';
 
 function App() {
+  // const img1 = pic;
   return (
     <div className="App">
       <header className="App-header">
         <h3>I ❤ U Monalisa!</h3>
+        <img src={pic}></img>
       </header>
     </div>
   );
