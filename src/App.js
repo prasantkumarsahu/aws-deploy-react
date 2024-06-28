00db1e948ce54e15b90973b61cab157c58362a55
Prasant Kumar Sahu <prasantkumarsahu0103@gmail.com>
2024-06-28 20:28:55 +0530

    added buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
 import pic from './pic-1.jpg';
+import { useState } from 'react';
 
 function App() {
-  // const img1 = pic;
+
+  const [accept, setAccept] = useState();
+
+  let header;
+  if (accept === true) {
+    header = <h3>I <span>❤</span> U Too Prasant!</h3>;
+  } else if (accept === false) {
+    header = <h3>💔</h3>;
+  } else {
+    header = "";
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h3>I <span>❤</span> U Monalisa!</h3>
         <img src={pic} alt="pic"></img>
+        <div className="buttons">
+          <button type="button" className="button-1" onClick={() => setAccept(true)}>Accept</button>
+          <button type="button" className="button-2" onClick={() => setAccept(false)}>Cancel</button>
+        </div>
+        {header}
       </header>
     </div>
   );
